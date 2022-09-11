60831b9a26ae3c0eba6587dddc109a4acbf25bc3
Dereje Desta <ddesta777@gmail.com>
2022-09-11 18:00:55 +0300

    done exercise 2.1 - 2.5 of part 2

---
+++ part-2/src/Total.js
@@ -0,0 +1,7 @@
+const Total = ({ parts }) => {
+  const total = parts.reduce((prev, curr) => prev + curr.exercises, 0)
+  return (
+    <b>total of {total} exercises</b>
+  )
+}
+export default Total
